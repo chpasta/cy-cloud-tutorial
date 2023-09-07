64cc46ef835e0b0b95d5cfd8b80c42ff77a3ae07
Carlos Cavalcante <cavalcante.carlos111@gmail.com>
2023-09-07 10:48:46 -0400

    comment projectId: 'p9d8j5',

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'p9d8j5',
+  // projectId: 'p9d8j5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
