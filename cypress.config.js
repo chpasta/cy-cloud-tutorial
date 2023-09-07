caa9119cd16a4606f3ddff90a82e9ec462dced3a
Carlos Cavalcante <cavalcante.carlos111@gmail.com>
2023-09-06 20:08:04 -0400

    First commint

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'p9d8j5',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
